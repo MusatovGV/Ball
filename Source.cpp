b1896cf3faa0e6f70ae19c4515d1bcca6840a33c
MusatovGV <81645624+MusatovGV@users.noreply.github.com>
2021-03-31 23:19:59 +0300

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,32 +1,34 @@
-#include <stdlib.h>
 #include "C:/Users/Георгий/Downloads/TX/TXLib.h" 
 #include "Ball.hpp"
 #include "Vector.hpp"
 #include "Drawing.hpp"
+#include <ctime>
 
 
 
 int main() 
 {
 
-	Drawing drawing(800, 800);
+	Drawing drawing(1980, 1080);
 
 	
-	txCreateWindow(drawing.wight_, drawing.hight_);
+	txCreateWindow(drawing.width_, drawing.height_);
 	drawing.Video_memory_ = txVideoMemory();
-	Ball ball(Vector{ 400, 300, 0 }, Vector{ 0, 0, 255 }, 50, 1);
+	Ball ball(Vector{ 400, 300, 0 }, Vector{ 0, 0, 0 }, Vector{ 0, 0, 0 }, Vector{ 0, 0, 255 }, 50, 1, 1);
 	//ball.draw(window1);
-	
 
-	
+	time_t t1 = time(NULL), t2 = 0;
 
-	while (!txGetAsyncKeyState(VK_ESCAPE))
+	while (!GetAsyncKeyState(VK_ESCAPE))
 	{
+		t2 = time(NULL)-t1;
+		t1 = time(NULL);
 		txBegin();
 		txClear();
-		ball.move();
+		ball.move(t2);
 		ball.draw(drawing);
 		txEnd();
+
 	}
 
 	
--- Vector.cpp
+++ Vector.cpp
@@ -5,3 +5,28 @@ Vector::Vector(double x, double y, double z) :
 	y_(y),
 	z_(z)
 {}
+
+Vector::Vector(double x):
+	x_(x),
+	y_(x),
+	z_(x)
+{}
+
+
+Vector Vector::operator * (const Vector& vec1)const
+{
+	return{ (*this).x_ * vec1.x_, (*this).y_ * vec1.y_,(*this).z_ * vec1.z_ };
+}
+
+Vector Vector::operator + (const Vector& vec1)const
+{
+	return{ (*this).x_ + vec1.x_, (*this).y_ + vec1.y_,(*this).z_ + vec1.z_ };
+}
+
+Vector& Vector::operator += (const Vector& vec1)
+{
+	(*this).x_ += vec1.x_;
+	(*this).y_ += vec1.y_;
+	(*this).z_ += vec1.z_;
+	return (*this);
+}
